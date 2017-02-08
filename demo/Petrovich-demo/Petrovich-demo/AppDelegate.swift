7da935d7848238fb33cde738353cca66eb287cec
Daniyar Salakhutdinov <daniyarsalakhutdinov@Mac-mini-Daniyar.local>
2017-02-08 15:12:18 +0300

    Created project.

---
+++ demo/Petrovich-demo/Petrovich-demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Petrovich-demo
+//
+//  Created by Daniyar Salakhutdinov on 08.02.17.
+//  Copyright © 2017 Runtime LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ demo/Petrovich-demo/Petrovich-demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Petrovich-demo
+//
+//  Created by Daniyar Salakhutdinov on 08.02.17.
+//  Copyright © 2017 Runtime LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
