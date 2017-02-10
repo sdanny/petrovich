8f42a1024846390ece03a008f978cb05b66b5742
Daniyar Salakhutdinov <daniyarsalakhutdinov@Mac-mini-Daniyar.local>
2017-02-10 18:25:17 +0300

    Demo project main view controller logic.

--- demo/Petrovich-demo/ViewController.swift
+++ demo/Petrovich-demo/ViewController.swift
@@ -9,19 +9,49 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet fileprivate var inputField:NSTextField!
+    @IBOutlet fileprivate var maleRadioButton: NSButton!
+    @IBOutlet fileprivate var femaleRadioButton: NSButton!
+    @IBOutlet fileprivate var genOutput: NSTextField!
+    @IBOutlet fileprivate var datOutput: NSTextField!
+    @IBOutlet fileprivate var accOutput: NSTextField!
+    @IBOutlet fileprivate var insOutput: NSTextField!
+    @IBOutlet fileprivate var preOutput: NSTextField!
+    
+    let petrovich = Petrovich.shared
+    var gender = Gender.male
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //
+        inputField.stringValue = "Иванов Иван Иванович"
+        processValue()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    // MARK: - actions
+    @IBAction func genderRadioButtonDidSelect(_ sender: NSButton) {
+        if sender === maleRadioButton {
+            gender = .male
+        } else {
+            gender = .female
         }
     }
-
-
+    
+    @IBAction func goButtonDidSelect(_ sender: NSButton) {
+        processValue()
+    }
+    
+    fileprivate func processValue() {
+        let components = inputField.stringValue.components(separatedBy: " ")
+        guard components.count == 3 else {
+            return
+        }
+        let lastname = components[0]
+        let firstname = components[1]
+        let middlename = components[2]
+        genOutput.stringValue = petrovich.lastname(lastname, gender: gender, declension: .genitive) + " " + petrovich.firstname(firstname, gender: gender, declension: .genitive) + " " + petrovich.middlename(middlename, gender: gender, declension: .genitive)
+        
+    }
 }
 
